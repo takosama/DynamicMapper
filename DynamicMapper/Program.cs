463adc05beebd529c61a4a48629b4d0fb6b3df5b
tako <takosama.312@gmail.com>
2017-10-31 12:40:50 +0900

    プロジェクト ファイルを追加します。

---
+++ DynamicMapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
