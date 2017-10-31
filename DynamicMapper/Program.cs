df4feeee1f08c1879fe5c3ccd9a4417a59f3370e
tako <takosama.312@gmail.com>
2017-10-31 13:46:21 +0900

    たぶん完成

--- DynamicMapper/Program.cs
+++ DynamicMapper/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DynamicMapper
 {
-    class Program
+    class Mapper
     {
-        static void Main(string[] args)
+        public static T Map<T>(dynamic obj) where T : new()
         {
+            var rtn = new T();
+
+            foreach (var n in obj)
+            {
+                var type = n.Key;
+                var val = n.Value;
+                try
+                {
+                    rtn.GetType().GetProperty(type).SetValue(rtn, val);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            return rtn;
         }
     }
 }
